c9d44c2ffd1a0804c48317a0bc8a80376e508033
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2017-08-05 00:10:27 +0300

    Container controller was enabled

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@
 import {Application, Request, Response} from "express-serve-static-core";
 
 import Constants from "./common/Constants";
+import ContainerController from './controllers/ContainerController';
 
 const path = require('path');
 const express = require('express');
@@ -12,6 +13,8 @@ const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(express.static(Constants.PUBLIC_DIR));
+
+new ContainerController(app);
 
 app.get('*', function (request: Request, response: Response) {
     response.sendFile(path.resolve(Constants.PUBLIC_DIR, 'index.html'));
