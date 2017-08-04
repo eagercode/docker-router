ae5d4d07457a867a8659700dc7a9f8fbd5d43d04
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2017-08-04 23:12:25 +0300

    Ajax utils were created

---
+++ client/src/utils/Utils.ts
@@ -0,0 +1,29 @@
+export default class Utils {
+
+    static ajaxGet<T>(url: string): Promise<T> {
+        let options = {
+            method: "GET"
+        };
+
+        return Utils.ajax(url, options);
+    }
+
+    static ajax<T>(url: string, options: any): Promise<T> {
+        return new Promise(function (resolve: (data: T) => void, reject: (error: any) => void) {
+            fetch(url, options).then((response: Response) => response.json().then((json: T) => ({
+                    status: response.status,
+                    json
+                })
+            )).then((response: { status: number, json: T }) => {
+                if (response.status >= 400) {
+                    reject(response.json);
+                } else {
+                    resolve(response.json);
+                }
+            }, function (error: any) {
+                reject(error);
+            });
+        });
+    }
+
+}
