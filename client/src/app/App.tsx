92e9f8fd4c9351cda83cb13d5f12c84261ad619f
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2017-08-04 22:13:27 +0300

    Small header changes

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,11 +6,8 @@ class App extends React.Component<{}, {}> {
         return (
             <div className="App">
                 <div className="App-header">
-                    <h2>Welcome to React</h2>
+                    <h2>Docker Router</h2>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
-                </p>
             </div>
         );
     }
