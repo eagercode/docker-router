801c70874651c48dd81ded337d23fb380c6b102a
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2017-08-07 22:43:55 +0300

    Additional method psAll was added into docker service

--- server/src/services/DockerService.ts
+++ server/src/services/DockerService.ts
@@ -8,8 +8,16 @@ export default class DockerService {
     }
 
     ps(): Promise<Container[]> {
+        return this.getContainers('docker ps');
+    }
+
+    psAll(): Promise<Container[]> {
+        return this.getContainers('docker ps -a');
+    }
+
+    private getContainers(command: string): Promise<Container[]> {
         return new Promise<Container[]>((resolve: (containers: Container[]) => void, reject: (error: string) => void): void => {
-            this.cliService.exec('docker ps')
+            this.cliService.exec(command)
                 .then((result: string): void => resolve(this.containerConverter.convertList(result)))
                 .catch((error: string): void => reject(error));
         });
