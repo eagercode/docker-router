a63480d94f8ef163cbb93d646e4f0af1f8fcb21f
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2017-08-13 11:04:33 +0300

    Development environment was added to the server-side

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,15 @@ const bodyParser = require('body-parser');
 
 const app: Application = express();
 
+if (process.env.NODE_ENV === 'development') {
+    app.use((req, res, next): void => {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    });
+}
+
 app.use(bodyParser.json());
 app.use(express.static(Constants.PUBLIC_DIR));
 
